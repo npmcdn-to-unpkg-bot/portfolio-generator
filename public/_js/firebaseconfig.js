6d5a92cb4f1a92372565219ee6b30f8e3eb490a6
andrewgremlich <andrew.gremlich@gmail.com>
2016-09-01 10:51:36 -0600

    Interface and Firebase integration update

---
+++ public/_js/firebaseconfig.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCoJYDEPTVm4C5DvZlITgnBizmxi3AS6wc",
+    authDomain: "portfolio-generator.firebaseapp.com",
+    databaseURL: "https://portfolio-generator.firebaseio.com",
+    storageBucket: "portfolio-generator.appspot.com",
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
